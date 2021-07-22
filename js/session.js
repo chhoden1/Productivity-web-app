2edc665b1743a2caff874d0e45ce5798c9f8e815
kaindick <kristin.a.indick@gmail.com>
2021-07-21 18:08:56 -0700

    Organized folders and added session form

---
+++ js/session.js
@@ -0,0 +1,26 @@
+/* For sidebar */
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let logoPlaceholder = document.querySelector("#logo-placeholder");
+
+closeBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("open");
+  menuBtnChange();//calling the function(optional)
+});
+
+// following are the code to change sidebar button(optional)
+function menuBtnChange() {
+ if(sidebar.classList.contains("open")){
+   closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the icons class
+   logoPlaceholder.classList.replace("bxs-square", "bx-check-circle");
+   logoPlaceholder.style.color = "#2e6873";
+
+ }else {
+   closeBtn.classList.replace("bx-menu-alt-right","bx-menu"); //replacing the icons class
+   logoPlaceholder.classList.replace("bx-check-circle", "bxs-square");
+   logoPlaceholder.style.color = "transparent";
+ }
+}
+
+/* For Session Title input */
+
